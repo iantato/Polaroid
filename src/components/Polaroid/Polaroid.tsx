9e97bd6a85edeb82c57a4cbfeaea207294d1d5d9
iantato <margiehouses@gmail.com>
2025-01-31 02:18:37 +0800

    properly made the polaroid

--- src/components/Polaroid/Polaroid.tsx
+++ src/components/Polaroid/Polaroid.tsx
@@ -1,28 +1,28 @@
 import Image from "next/image";
+import { useMemo } from "react";
 
 interface PolaroidProps {
   src: string;
   alt: string;
-  caption?: string;
 }
 
-export const Polaroid = ({ src, alt, caption }: PolaroidProps) => {
+export const Polaroid = ({ src, alt }: PolaroidProps) => {
+  const rotation = useMemo(() => {
+    return Math.floor(Math.random() * 11) - 5;
+  }, []);
+
   return (
-    <div className="bg-white p-4 shadow-lg rotate-2 w-[300px]">
+    <div className="bg-white p-4 pb-16 shadow-lg w-[300px]" style={{ transform: `rotate(${rotation}deg)` }}>
       <div className="relative w-full aspect-square mb-2">
         <Image
           src={src}
           alt={alt}
           fill
-          className="object-cover"
+          draggable="false"
+          className="object-cover select-none"
           sizes="300px"
         />
       </div>
-      {caption && (
-        <div className="text-center font-mono text-sm pb-2">
-          {caption}
-        </div>
-      )}
     </div>
   );
 };
