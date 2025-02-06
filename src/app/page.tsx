b9deb29e1d4ac8fede1a9777cb12ca2b585be331
iantato <margiehouses@gmail.com>
2025-02-06 20:29:36 +0800

    Added .env.example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-'use client'
 import { Polaroid } from "@/components/Polaroid";
 
-export default function Home() {
-
+export default async function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center p-8 overflow-hidden">
       <Polaroid
