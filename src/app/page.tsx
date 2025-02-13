0f3a2f0eeee5e708d1f572d9eaf42f44510b1185
iantato <margiehouses@gmail.com>
2025-02-14 02:23:41 +0800

    Removed some unecessary imports.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import { PolaroidGrid } from "@/components/PolaroidGrid";
-import { SearchParams } from '@/types';
 
-export default async function Home({ id }: SearchParams) {
-
-
+export default async function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center p-8 overflow-hidden">
       <PolaroidGrid></PolaroidGrid>
